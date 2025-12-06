b3ff08cab769e5e977b6ff8bfaf0d5507ddd0d12
Vejmon <vemundhell@gmail.com>
2025-12-06 16:45:42 +0100

    Aktiverte JPA Auditing og la til avhengighet for Spring Data JPA i prosjektet

--- src/main/java/no/vejmon/dommern/DommernApplication.java
+++ src/main/java/no/vejmon/dommern/DommernApplication.java
@@ -2,7 +2,9 @@ package no.vejmon.dommern;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class DommernApplication {
     public static void main(String[] args) {
