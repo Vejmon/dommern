d6dcd9be0e221f4b9e5454029c99ccc8d25be3d3
Vejmon <vemundhell@gmail.com>
2025-12-06 16:34:33 +0100

    fjernet Groovy

---
+++ src/main/java/no/vejmon/dommern/DommernApplication.java
@@ -0,0 +1,12 @@
+package no.vejmon.dommern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DommernApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DommernApplication.class, args);
+    }
+
+}
