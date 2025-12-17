b7b8bcbfca85a976ca01eb0b2c2857ae23cb7b2d
Vejmon <vemundhell@gmail.com>
2025-12-17 16:26:48 +0100

    Legger til `@PreDestroy` og oppdaterer oppstart i `GPIOLytter`
    Refaktorerer `GPIOLytter` til å inkludere logging ved oppstart og opprydding i `@PostConstruct` og `@PreDestroy`. Flytter `baneMap` til å være en instansevariabel og legger til logging for opprettelse av knapper.

--- src/main/java/no/vejmon/dommern/lyttere/GPIOLytter.java
+++ src/main/java/no/vejmon/dommern/lyttere/GPIOLytter.java
@@ -7,6 +7,7 @@ import com.pi4j.io.gpio.digital.DigitalInputConfigBuilder;
 import com.pi4j.io.gpio.digital.DigitalState;
 import com.pi4j.io.gpio.digital.PullResistance;
 import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import lombok.extern.slf4j.Slf4j;
 import no.vejmon.dommern.bane.BaneType;
 import no.vejmon.dommern.bane.Runde;
@@ -22,17 +23,19 @@ import java.util.Map;
 public class GPIOLytter implements Lytter {
 
     private final ApplicationEventPublisher publisher;
-    private static Map<BaneType, Integer> baneMap;
+    private final Map<BaneType, Integer> baneMap;
     private static Context pi4j;
 
-    public GPIOLytter(ApplicationEventPublisher publisher) {
+    public GPIOLytter(ApplicationEventPublisher publisher,
+                      Map<BaneType, Integer> baneMap) {
         this.publisher = publisher;
+        this.baneMap = baneMap;
     }
 
     @PostConstruct
     void init(){
         pi4j = Pi4J.newAutoContext();
-
+        log.info("Starting GPIO Lytter: {}", pi4j.describe().name());
         baneMap.entrySet().forEach( baneMapEntry -> {
             DigitalInputConfigBuilder buttonConfig = DigitalInput.newConfigBuilder(pi4j)
                     .id("button")
@@ -41,17 +44,22 @@ public class GPIOLytter implements Lytter {
                     .pull(PullResistance.PULL_DOWN)
                     .debounce(3000L);
             DigitalInput button = pi4j.create(buttonConfig);
+            log.info("Created button: {}", button.getName());
 
             button.addListener(event -> {
                 if (event.state() == DigitalState.LOW){
                     Runde runde = new Runde(baneMapEntry.getKey());
                     publisher.publishEvent(new NyRundeEvent(this, runde));
-
                 }
             });
 
         });
+    }
 
+    @PreDestroy
+    public void teardown(){
+        log.info("Tearing down GPIO Lytter: {}", pi4j.describe().name());
+        pi4j.shutdown();
     }
 
 }
