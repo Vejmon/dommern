fd66ef0149e82f39f98dc40eac6c097e27d6e5d7
Vejmon <vemundhell@gmail.com>
2025-12-18 09:06:04 +0100

    Legger til støtte for Testcontainers for PostgreSQL i tester
    Oppdaterer testklassen `DommernApplicationTests` med Testcontainers og konfigurerer PostgreSQL-container. Forenkler `application.yml` ved å fjerne databasekonfigurasjon som nå håndteres av Testcontainers.

--- src/test/java/no/vejmon/dommern/DommernApplicationTests.java
+++ src/test/java/no/vejmon/dommern/DommernApplicationTests.java
@@ -2,12 +2,22 @@ package no.vejmon.dommern;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 
+@Testcontainers
 @SpringBootTest
 public class DommernApplicationTests {
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres =
+            new PostgreSQLContainer<>("postgres:17.4");
+
+
     @Test
     public void contextLoads() {
     }
-
 }
