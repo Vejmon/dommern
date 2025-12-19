5cb413239225ad13d67c2c9065a2c3682e994704
Vejmon <vemundhell@gmail.com>
2025-12-19 23:39:49 +0100

    Skjuler `findTopByKuskOrderByStartDesc` og `findByName` fra Spring Data REST
    Legger til `@RestResource(exported = false)` på metodene `findTopByKuskOrderByStartDesc` og `findByName` i henholdsvis `RundeRepository` og `KuskRepository` for å forhindre eksponering via Spring Data REST.

--- src/main/java/no/vejmon/dommern/bane/KuskRepository.java
+++ src/main/java/no/vejmon/dommern/bane/KuskRepository.java
@@ -2,6 +2,7 @@ package no.vejmon.dommern.bane;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,6 +11,7 @@ import java.util.UUID;
 @Repository
 public interface KuskRepository extends JpaRepository<Kusk, UUID> {
 
+    @RestResource(exported = false)
     Optional<Kusk> findByName(String name);
 
 }
