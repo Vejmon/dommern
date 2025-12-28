dd77df4e1c9969fc6720de4e079b0143b8e85ac1
Vejmon <vemundhell@gmail.com>
2025-12-28 09:13:57 +0100

    restart systemctl and wait for service to be up.

--- service_start.sh
+++ service_start.sh
@@ -4,5 +4,4 @@ git pull --rebase
 docker compose down
 docker image prune -fa
 docker compose pull dommern
-docker compose up -d dommern
-curl --fail --retry 20 --retry-delay 5 http://localhost:8080/actuator/health
+docker compose up dommern
