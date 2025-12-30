d233df622cd7118d175f94585425b24ac02028e7
Vejmon <vemundhell@gmail.com>
2025-12-30 19:43:56 +0100

    app: brakt inn aktive kusker fra linjeDommer sine kusker.

---
+++ src/main/java/no/vejmon/dommern/view/ViewController.java
@@ -0,0 +1,22 @@
+package no.vejmon.dommern.view;
+
+import no.vejmon.dommern.judge.LinjeDommer;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+
+    private final LinjeDommer linjeDommer;
+
+    public ViewController(LinjeDommer linjeDommer) {
+        this.linjeDommer = linjeDommer;
+    }
+
+    @GetMapping({"/", "/index", ""})
+    public String index(Model model) {
+        model.addAttribute("kusker", linjeDommer.getKusker());
+        return "index";
+    }
+}
