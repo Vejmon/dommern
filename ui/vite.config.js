7a64e7b16e6ba7d606d14e83945af77034d54209
Vejmon <vemundhell@gmail.com>
2026-01-02 19:39:54 +0100

    ui: setup local and index builds.

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -5,31 +5,25 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === 'production';
+
+  return {
+    plugins: [
+      vue(),
+      vueDevTools(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     },
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        // JS entry files
-        entryFileNames: 'js/[name].[hash].js',
-        // JS chunk files (for code splitting)
-        chunkFileNames: 'js/[name].[hash].js',
-        // CSS and other asset files
-        assetFileNames: ({name}) => {
-          if (name && name.endsWith('.css')) {
-            return 'css/[name].[hash][extname]'
-          }
-          return 'assets/[name].[hash][extname]'
-        },
+    build: {
+      rollupOptions: {
+        input: isProduction
+          ? { main: 'index.html' }
+          : { main: 'local.html' }
       }
     }
   }
-})
+});
