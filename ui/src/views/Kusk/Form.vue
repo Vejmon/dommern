fd78d75af76f32478848d88a73fbe7da823e77c2
Vejmon <vemundhell@gmail.com>
2026-01-14 12:38:41 +0100

    ui: add form submission functionality and API integration for Kusk

--- ui/src/views/Kusk/Form.vue
+++ ui/src/views/Kusk/Form.vue
@@ -1,10 +1,19 @@
 <script setup lang="ts">
 import {ref} from "vue";
 import {postData} from "@/utils/api.js";
+import {BaneType} from "@/types/BaneType";
 
 const formRef = ref<HTMLFormElement | null>(null);
+const currentBane = ref<BaneType | null>(null);
+const selectableBanes: BaneType[] = [
+    BaneType.KORTESTE_VEIEN,
+    BaneType.FRODE_SPESIAL,
+    BaneType.UTEN_NAVN,
+    BaneType.BESTEFAR_BANEN
+];
 
 const submitForm = async () => {
+  console.log('ref.value', formRef.value);
   if (!formRef.value) return;
   const formData = new FormData(formRef.value);
   const data = Object.fromEntries(formData.entries());
@@ -17,26 +26,33 @@ const submitForm = async () => {
     });
 }
 
-
-
 </script>
 
 <template>
   <div>
-    <form id="kuskForm">
-      <div>
+    <form id="kuskForm" ref="formRef" class="input-form pb-32 max-w-2xl">
+      <div class="input-group">
         <label for="name">Navn:</label>
-        <input type="text" id="name" name="name" />
+        <input class="input-text" type="text" id="name" name="name" />
       </div>
-      <div>
+      <div class="input-group">
         <label for="email">Epost:</label>
-        <input type="text" id="email" name="email" />
+        <input class="input-text" type="text" id="email" name="email" />
       </div>
-      <div>
+      <div class="input-group">
         <label for="currentBane">Bane:</label>
-        <input type="text" id="currentBane" name="currentBane" />
+        <select id="currentBane" name="currentBane" v-model="currentBane" class="input-text">
+          <option :value="null"></option>
+          <option
+              v-for="bane in selectableBanes"
+              :key="bane"
+              :value="bane"
+          >
+            {{ bane }}
+          </option>
+        </select>
       </div>
-      <button class="hover:cursor-pointer-" type="button" @click="submitForm">Submit</button>
+      <button class="w-full input-text hover:cursor-pointer" type="button" @click="submitForm">Submit</button>
     </form>
   </div>
 </template>
